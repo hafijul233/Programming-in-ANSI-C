751a5801b73c969126289554dcd3008a4614fc56
Hafijul Islam <hafijul233@users.noreply.github.com>
2018-05-07 02:11:59 +0600

    Chapter 1

--- 01 - Overview of C/1.15 - Simple Arithmetic Calculator.c
+++ 01 - Overview of C/1.15 - Simple Arithmetic Calculator.c
@@ -4,12 +4,12 @@ int main()
 {
     int x, y;
 
-    x =100;
-    y = 200;
+    x =10;
+    y = 20;
 
-    printf("X\t: %d    Y\t: %d\n",x,y);
-    printf("Sum\t: %d    Difference\t: %d\n",x+y,x-y);
-    printf("Product\t: %d    Division\t: %0.3f\n",x*y,x/y);
+    printf("X\t\t: %d\tY\t\t: %d\n",x,y);
+    printf("Sum\t\t: %d\tDifference\t: %d\n",x+y,x-y);
+    printf("Product\t\t: %d\tDivision\t: %0.3f\n",x*y,x/y);
 
 
     return 0;
