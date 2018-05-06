a70701c97de9e6a84cf7dd11bbdfa543dc9d2f11
Hafijul Islam <hafijul233@users.noreply.github.com>
2018-05-07 02:12:19 +0600

    Chapter 4

---
+++ 04 - Managing IO Operation/4.1 - WordProecessing.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  int counter = 0;
+
+  char text1[] = "WORD", text2[] = "PROCESSING",text3[] = "W.P.";
+
+  printf("%s %s\n",text1,text2);
+  printf("%s\n%s\n",text1,text2);
+  printf("%s\n",text3);
+
+
+
+    return 0;
+}
